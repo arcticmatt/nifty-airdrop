11b67c6ff2e5434d081308067520eb46f406e839
Matt Lim <mlimcollege@gmail.com>
2021-10-06 23:47:52 -0700

    Fix font link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500display=swap"
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap"
           rel="stylesheet"
         />
 
