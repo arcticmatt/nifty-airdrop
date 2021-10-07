b13715211dcd1ff46c24b9cb648bde4699ad3bee
Matt Lim <mlimcollege@gmail.com>
2021-10-06 21:55:09 -0700

    Add some metadata to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,30 @@ import "@/css/global/fonts/FontClasses.css";
 import "@/css/global/fonts/FontVariables.css";
 
 import { AppProps } from "next/app";
+import Head from "next/head";
+
+const META_DESCRIPTION = "An airdrop tool for cryptoartists";
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500display=swap"
+          rel="stylesheet"
+        />
+
+        <meta name="description" content={META_DESCRIPTION} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
